9f1063488788a869845e7d17b354e4ab41172bab
marco rizzoli <marco@rizzoli.me.uk>
2015-04-25 18:54:42 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #variables:
 repo="https://github.com/mrizzoli/mrizzoli.github.io.git"
-dest="/home/marco/mrizzoli.github.io/test"
+dest="/home/marco/mrizzoli.github.io/deploy"
 src="/home/marco/mrizzoli.github.io/src/_site"
 
 if [ ! -d "$dest" ]; then
-   echo "cloning "$repo" into "$dir"" && git clone $repo $dest
+   echo "cloning "$repo" into "$dir" " && git clone $repo $dest
 
 else
     echo "mi metto in pari con upstream" && cd $dest && git pull origin master
 fi
 
-#rsync -av $src/ $dest
-#cd $dest && git add . && git commit -m "update" && git push origin master 
+echo "syncing "$src" with "$dest" "
+
+rsync -av $src/ $dest
+
+cd $dest && git add . && git commit -m "update" && git push origin master 
