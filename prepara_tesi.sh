9fc145e5818972221d6374a1dd49792550975850
mrizzoli <marco@rizzoli.me.uk>
2015-10-27 16:47:48 +0100

    nogit

--- prepara_tesi.sh
+++ prepara_tesi.sh
@@ -13,17 +13,34 @@ mkdir -p ${src}
 mkdir -p ${temporanea}
 mkdir -p ${dest}
 
+##aws cli
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install awscli
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+##configura aws
+
+export AWS_ACCESS_KEY_ID=${aws-id}
+export AWS_SECRET_ACCESS_KEY=${aws-key}
+export AWS_SECRET_ACCESS_KEY=${aws-region}
+
+
 ##scarica file sorgente
 cd ${src}
 
-wget -O benenati.bib ${benenati}
-wget -O Chapter1.md ${cap1}
-wget -O Chapter2.md ${cap2}
-wget -O Chapter3.md ${cap3}
-wget -O Chapter4.md ${cap4}
-wget -O Chapter5.md ${cap5}
-wget -O Chapter6.md ${cap6}
-wget -O note.md ${note}
+aws s3 sync s3://tesi-src .
+
+#wget -O benenati.bib ${benenati}
+#wget -O Chapter1.md ${cap1}
+#wget -O Chapter2.md ${cap2}
+#wget -O Chapter3.md ${cap3}
+#wget -O Chapter4.md ${cap4}
+#wget -O Chapter5.md ${cap5}
+#wget -O Chapter6.md ${cap6}
+#wget -O note.md ${note}
 
 ##concatena md
 
