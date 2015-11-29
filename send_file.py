cc7a089690947c5a3d9d2734fa8a48c60843d33b
marco rizzoli <mrizzoli@localhost.localdomain>
2015-11-29 12:42:06 -0500

    noname

--- send_file.py
+++ send_file.py
@@ -38,4 +38,4 @@ def mail(to, subject, text, attach):
 mail("marcosofista@gmail.com",
    "Hello from python!",
    "This is a email sent with python",
-     newname)
+    "/tmp/tesi_wd/template_tesi.pdf")
