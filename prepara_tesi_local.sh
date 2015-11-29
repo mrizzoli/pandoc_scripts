ce6e09136e36c4709a85f0d62504d2ac62b3e9b2
marco rizzoli <mrizzoli@localhost.localdomain>
2015-11-29 03:04:16 -0500

    update

--- prepara_tesi_local.sh
+++ prepara_tesi_local.sh
@@ -45,4 +45,7 @@ xelatex template_tesi.tex
 
 mv ${dest}/template_tesi.pdf ~/Documents/tesi_"$(date +%F_%T)".pdf
 
+wget -O send_file.py https://raw.githubusercontent.com/mrizzoli/pandoc_scripts/master/send_file.py
+python send_file.py
+
 rm -rf ${dest}
