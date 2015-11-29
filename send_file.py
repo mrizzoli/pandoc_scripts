53e203414e5e90a3da2af70d00e222c6a318687b
marco rizzoli <mrizzoli@localhost.localdomain>
2015-11-29 14:24:10 -0500

    empty

--- send_file.py
+++ send_file.py
@@ -10,7 +10,37 @@ import os
 mail_user = "marco@rizzoli.me.uk"
 mail_pwd = os.getenv('mail_pwd')
 
-def mail(to, subject, text, attach):
+# def mail(to, subject, text, attach):
+#    msg = MIMEMultipart()
+
+#    msg['From'] = mail_user
+#    msg['To'] = to
+#    msg['Subject'] = subject
+
+#    msg.attach(MIMEText(text))
+
+#    part = MIMEBase('application', 'octet-stream')
+#    part.set_payload(open(attach, 'rb').read())
+#    Encoders.encode_base64(part)
+#    part.add_header('Content-Disposition',
+#            'attachment; filename="%s"' % os.path.basename(attach))
+#    msg.attach(part)
+
+#    mailServer = smtplib.SMTP("smtp.zoho.com", 587)
+#    mailServer.ehlo()
+#    mailServer.starttls()
+#    mailServer.ehlo()
+#    mailServer.login(mail_user, mail_pwd)
+#    mailServer.sendmail(mail_user, to, msg.as_string())
+#    # Should be mailServer.quit(), but that crashes...
+#    mailServer.close()
+
+# mail("marcosofista@gmail.com",
+#    "Hello from python!",
+#    "This is a email sent with python",
+#     "/tmp/tesi_wd/template_tesi.pdf")
+
+def mail(to, subject, text):
    msg = MIMEMultipart()
 
    msg['From'] = mail_user
@@ -37,5 +67,4 @@ def mail(to, subject, text, attach):
 
 mail("marcosofista@gmail.com",
    "Hello from python!",
-   "This is a email sent with python",
-    "/tmp/tesi_wd/template_tesi.pdf")
+     "This is a email sent with python")
