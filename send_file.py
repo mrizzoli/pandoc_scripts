99ba66a4b15695b9dea3a07442f9397f2d966157
marco rizzoli <mrizzoli@localhost.localdomain>
2015-11-29 21:44:12 -0500

    socket timeout

--- send_file.py
+++ send_file.py
@@ -26,7 +26,7 @@ def mail(to, subject, text, attach):
            'attachment; filename="%s"' % os.path.basename(attach))
    msg.attach(part)
 
-   mailServer = smtplib.SMTP("smtp.zoho.com", 587)
+   mailServer = smtplib.SMTP("smtp.zoho.com", 587, 300)
    mailServer.ehlo()
    mailServer.starttls()
    mailServer.ehlo()
