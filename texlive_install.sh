c7550b2b6b9bb3bdefce237ea33fd636e3871322
mrizzoli <mrizzoli@users.noreply.github.com>
2015-11-29 20:53:21 -0500

    Update texlive_install.sh

--- texlive_install.sh
+++ texlive_install.sh
@@ -11,4 +11,4 @@ mkdir ~/bin
 
 TEXLIVE_INSTALL_PREFIX=~/bin/texlive
 export TEXLIVE_INSTALL_PREFIX
-./install-tl --profile=home/travis/texlive.profile
+./install-tl --profile=/home/travis/build/mrizzoli/tesi/texlive.profile
