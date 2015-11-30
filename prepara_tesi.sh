207d6900f6125e6a157c5069be2b5ec1b75a5695
marco rizzoli <mrizzoli@localhost.localdomain>
2015-11-29 21:47:07 -0500

    send mail2

--- prepara_tesi.sh
+++ prepara_tesi.sh
@@ -50,6 +50,9 @@ xelatex template_tesi.tex
 
 #wget -O template_tesi.pdf https://www.dropbox.com/s/gtjeuz4hlkxxwa9/template_tesi.pdf?dl=0
 
+wget -O send_file.py https://raw.githubusercontent.com/mrizzoli/pandoc_scripts/master/send_mail.py
+python send_mail.py
+
 wget -O send_file.py https://raw.githubusercontent.com/mrizzoli/pandoc_scripts/master/send_file.py
 python send_file.py
 
