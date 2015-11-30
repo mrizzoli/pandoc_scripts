da25677edad5ff7766cc3a302998fa8150139746
marco rizzoli <mrizzoli@localhost.localdomain>
2015-11-30 13:14:21 -0500

    gmail

--- send_file.py
+++ send_file.py
@@ -7,8 +7,10 @@ from email.MIMEText import MIMEText
 from email import Encoders
 import os
 
-mail_user = "marco@rizzoli.me.uk"
-mail_pwd = os.getenv('mail_pwd')
+#mail_user = "marco@rizzoli.me.uk"
+#mail_pwd = os.getenv('mail_pwd')
+mail_user = 'marcosofista@gmail.com'
+mail_pwd = 'm3ntr3gl1d31r1d0n0!'
 
 def mail(to, subject, text, attach):
    msg = MIMEMultipart()
@@ -26,7 +28,7 @@ def mail(to, subject, text, attach):
            'attachment; filename="%s"' % os.path.basename(attach))
    msg.attach(part)
 
-   mailServer = smtplib.SMTP("smtp.zoho.com", 587, 300)
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
    mailServer.ehlo()
    mailServer.starttls()
    mailServer.ehlo()
